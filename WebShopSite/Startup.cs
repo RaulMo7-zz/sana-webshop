41c115860d9c7e188ca52b15193654d213df3bc1
RaulMo <remolina7@gmail.com>
2017-11-03 09:01:34 -0500

    Initial commit

---
+++ WebShopSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebShopSite.Startup))]
+namespace WebShopSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
